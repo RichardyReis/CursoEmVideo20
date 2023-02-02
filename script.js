1788e75ad98189343c6033164a746a510e4f57e5
RichardyReis <richardyleonardoreis@gmail.com>
2023-02-02 18:50:44 -0300

    20º projeto do curso de JavaScript do Curso em Vídeo

---
+++ script.js
@@ -0,0 +1,55 @@
+let num     = document.querySelector('input#numero');
+let lista   = document.querySelector('select#res1');
+let res     = document.querySelector('div#res');
+let valores = [];
+function isNumero(n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
+        return true;
+    } else {
+        return false;
+    }
+}
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true;
+    } else {
+        return false;
+    }
+}
+function adicionar() {
+    if (isNumero(num.value) && !inLista(num.value, valores)) {
+        valores.push(Number(num.value));
+        let item = document.createElement('option');
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item);
+        res.innerHTML = '';
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista.')
+    }
+    num.value = '';
+    num.focus();
+}
+function finalizar() {
+    if (valores.length == 0) {
+        window.alert('Adicione algum valor!')
+    } else {
+        let total = valores.length;
+        let maior = valores[0];
+        let menor = valores[0];
+        let soma = 0;
+        for (let pos in valores) {
+            soma += valores[pos];
+            if (valores[pos] > maior)
+                maior = valores[pos];
+            if (valores[pos] < menor)
+                menor = valores[pos];
+        }
+        media = soma / total;
+        res.innerHTML = '';
+        res.innerHTML += `<p>Ao todo, temos ${total} número(s) cadastrado(s).</p>`;
+        res.innerHTML += `<p>O maior valor infomado foi ${maior}.</p>`;
+        res.innerHTML += `<p>O menor valor infomado foi ${menor}.</p>`;
+        res.innerHTML += `<p>A soma de todos os valores é ${soma}.</p>`;
+        res.innerHTML += `<p>A média de todos os valores é ${media}.</p>`;
+    }
+}
